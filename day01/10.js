e0c4d49c82bf12c991c0cc8a02e12138df3dc095
李白 <liujunhangWX@163.com>
2017-04-17 23:06:07 +0800

    ES6 study

---
+++ day01/10.js
@@ -0,0 +1,3 @@
+export const a = 'hello,world';
+export const b = 'today is good day';
+export const c = 'look up';
---
+++ day01/12.js
@@ -0,0 +1,4 @@
+"use strict";
+
+import * as cont from './';
+console.log(cont.a);
